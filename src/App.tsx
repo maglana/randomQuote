209257feb02aefa2ff13fe9e84016a5f2f506342
maglana <m.srutkowska@gmail.com>
2023-12-13 11:52:12 +0100

    img and first mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import "./App.css";
-import { Button } from "@mui/material";
+import { Box } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+
+const Item = styled(Paper);
 
 function App() {
 	return (
-		<div>
-			<p>Hello</p>
-			<Button variant="contained">test</Button>
-		</div>
+		<Box>
+			<Grid
+				container
+				spacing={2}
+			>
+				<Grid
+					item
+					xs={6}
+				>
+					1
+				</Grid>
+				<Grid
+					item
+					xs={6}
+				>
+					2
+				</Grid>
+			</Grid>
+		</Box>
 	);
 }
 
